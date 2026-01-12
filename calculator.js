c8e237d08094ca2c57f1fc47c990dd7479f9f41e
Tryston <trystonproffit@gmail.com>
2026-01-12 13:58:40 -0500

    Fixed some issues, should be complete

--- calculator.js
+++ calculator.js
@@ -5,28 +5,33 @@ const calculate = {
 add(a, b){   //Created the "add" function to add two numbers, save the input to history, and return the answer
     const answer = a + b;  
     this.history.push(`${a} + ${b} = ${answer}`);
+    console.log(answer);
     return answer
 },
 
 subtract(a, b){   //Created the "subtract" function to subtract two numbers, save the input to history, and return the answer
     const answer = a - b;
     this.history.push(`${a} - ${b} = ${answer}`);
+    console.log(answer);
     return answer;
 },
 
 multiply(a, b){ //Created the "multiply" function to multiply two numbers, save the input to history, and return the answer
     const answer = a * b;
     this.history.push(`${a} x ${b} = ${answer}`)
+    console.log(answer);
     return answer;
 },
 
 divide(a, b){ //Created the "divide" function to divide two numbers, save the input to history, and return the answer
     if(b === 0){  //Used if to return an error if the user inputs and number divided by 0
         this.history.push(`Division by zero attempted: ${a} / ${b}`);
+        console.log("Error: Division by Zero");
         return "Error: Division by Zero";
     }
     const answer = a / b;
     this.history.push(`${a} / ${b} = ${answer}`);
+    console.log(answer);
     return answer;
 },
 
@@ -50,5 +55,7 @@ calculate.subtract(136, 36);
 calculate.multiply(6, 6);
 calculate.divide(100, 4);
 calculate.divide(3, 0);
+
+calculate.multiply(12, 48);
 
 calculate.viewHistory();
